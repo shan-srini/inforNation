1f565ceedf9e863903776cb85244089c23c6a03a
jaqpet123 <jaqpet123@gmail.com>
2022-04-01 14:28:39 -0400

    Added recommended articles

---
+++ src/content/articles/recommended/16.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+const article = {
+  id: 16,
+  title: "Odd Performance Art to Celebrate Sesame Stream Novel",
+  image:
+    "https://media.istockphoto.com/photos/large-bird-costume-with-pet-person-picture-id108224244?k=20&m=108F224244&s=170667a&w=0&h=XPGdj93dAi2PJ7jbR1SFWkOIQprZZAmfzyXLr9cyDiw=FF",
+  content: (
+    <div>
+      <p>
+        In a perhaps misguided attempt to celebrate the new Sesame Street novel
+        “Big Bird and You,” Swedish performance artists Kyle and Louise Krane
+        uploaded a video of them performing a meta-interpretation of the novel's
+        plot. Mr. Krane donned a Big Bird-like costume, and proceeded to read it
+        to Mrs. Krane, who was locked in a cage. The demonstration lasted eight
+        hours, letting the pair read through the book in whole. There is
+        evidence of video-splicing during the video, indicating that the pair
+        might have taken breaks so as to not be locked into the performance for
+        the whole duration.
+      </p>
+      <p>
+        The Kranes, upon questioning, had both much and nothing to say about
+        their performance art.
+      </p>
+      <p>
+        Kyle: “Me in the costume and Lou in the cage is a meta-commentary on the
+        intensity that Big Bird brings to the novel.”
+      </p>
+      <p>
+        Louise: “In a way, the reader is held captive by the novel, enraptured
+        and forced to read and read until conclusion.”
+      </p>
+      <p>
+        Kyle: “We think it's a kind of meta-commentary on the way politics is
+        all around us, never letting us be free from the weight of society.”
+      </p>
+      <p>
+        You can find the Kranes' video on the internet, and “Big Bird and You”
+        in bookstores everywhere.
+      </p>
+    </div>
+  ),
+};
+
+export default article;
---
+++ src/content/articles/recommended/19.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+const article = {
+  id: 19,
+  title: "What To Do When Your Child Discovers Fanfiction",
+  image:
+    "https://edit.co.uk/uploads/2016/12/Image-1-Alternatives-to-stock-photography-Thinkstock.jpg",
+  content: (
+    <div>
+      <p>
+        As any parent of a teenager can tell you, one of the scariest moments is
+        when their beloved bundle of joy learns enough to navigate the web on
+        their own. One day, your child might find themselves on a website
+        dedicated to fanfiction. What do you do then? Have a serious talk with
+        them, or pretend you didn't notice as they explore? Researchers from
+        Golden Glizzy University think they might have come up with just the
+        answer.
+      </p>
+      <p>
+        The researchers from GGU, led by Dr. Andromeda Anastasia, have come to
+        the conclusion that the best thing to do is to dredge up your own
+        embarrassing fanfiction, and have a heart-to-heart with your child.
+        Assure them that this is perfectly natural and nothing to be embarrassed
+        about, but make sure they understand the need to tread carefully. Make
+        sure they download an antivirus to ensure proper protection, and have
+        them ask “would these characters really do this” before reading so that
+        their beloved protagonists are not tainted by poorly-written stories.
+        After this, assure them that you'll be available to talk to, and let
+        them have their privacy.
+      </p>
+    </div>
+  ),
+};
+
+export default article;
