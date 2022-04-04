bee97093954aa999665870b48b7c71cad42d6bb4
Jack Carson <jackalakalaka@gmail.com>
2022-04-04 14:42:29 -0400

    Jack initial tweaks

--- src/pages/article/Article.js
+++ src/pages/article/Article.js
@@ -2,11 +2,30 @@ import { CircularProgress, Typography, Container } from '@mui/material';
 import './Article.css';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import {getArticleById} from '../../content/articles';
+import { getArticleById } from '../../content/articles';
 
 const Article = (props) => {
     const { id: articleId } = useParams();
     const [article, setArticle] = useState();
+    const [isVizOpen, setIsVizOpen] = useState(false)
+
+    const VizToggle = ({ isVizOpen }) => {
+        return isVizOpen ?
+            (
+                <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-minus" width={24} height={24} viewBox="0 0 24 24" stroke-width={2} stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                    <line x1={5} y1={12} x2={19} y2={12}></line>
+                </svg>
+            )
+            :
+            (
+                <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-plus" width={24} height={24} viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                    <line x1={12} y1={5} x2={12} y2={19}></line>
+                    <line x1={5} y1={12} x2={19} y2={12}></line>
+                </svg>
+            )
+    }
 
     useEffect(() => {
         getArticleById(articleId)
@@ -20,14 +39,15 @@ const Article = (props) => {
 
     return (
         article ?
-        <Container className='article-container'>
-            <Typography variant='h3' textAlign='center'>{article.title}</Typography>
-            <Container className='article-content'>
-                {article.content}
+            <Container className='article-container'>
+                <Typography className='article-title' variant='h3' textAlign='left'>{article.title}</Typography>
+
+                <Container className='article-content'>
+                    {article.content}
+                </Container>
             </Container>
-        </Container>
-        :
-        <CircularProgress />
+            :
+            <CircularProgress />
     )
 }
 
