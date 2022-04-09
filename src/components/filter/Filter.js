c8613d03453c50b8dff23fd3a8f8734abd451b19
Shan Srinivasan <s.shan.srini@gmail.com>
2022-04-08 23:34:18 -0400

    oops! lint

--- src/components/filter/Filter.js
+++ src/components/filter/Filter.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./Filter.css";
 import { Popover, Slider, Stack, Divider, TextField } from "@mui/material";
 import FilterListIcon from "@mui/icons-material/FilterList";
